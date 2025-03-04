df343241e4dab332c0316f0ce188c177dd8ddc8a
tan1 <peit879@gmail.com>
2025-03-04 20:39:52 +0800

    Update build_dataset.py

--- build_dataset.py
+++ build_dataset.py
@@ -1,4 +1,56 @@
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 import torch
+from torchvision import transforms, datasets
+from sklearn.model_selection import train_test_split
 
-data_dir = ''
+data_dir = 'FcccccUestc/Training_data'
+
+# 图像变换
+train_transforms = transforms.Compose([
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomVerticalFlip(p=0.5),
+    transforms.RandomRotation(degrees=35),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+valid_transforms = transforms.Compose([
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+# -------------------------------------------------------------------------------------------------
+
+# 数据集划分（80%训练，20%验证）
+
+dataset = datasets.ImageFolder(root=data_dir, transform=None)
+data_len = len(dataset)
+
+train_size = int(0.8 * data_len)
+val_size = data_len - train_size
+train_indices, val_indices = torch.utils.data.random_split(
+    range(data_len), [train_size, val_size]
+)
+
+
+class TransformSubset(torch.utils.data.Dataset):
+    def __init__(self, subset, transform=None):
+        self.subset = subset
+        self.transform = transform
+
+    def __getitem__(self, index):
+        x, y = self.subset[index]
+        if self.transform:
+            x = self.transform(x)
+        return x, y
+
+    def __len__(self):
+        return len(self.subset)
+
+
+# 应用不同的 Transform
+train_dataset = TransformSubset(train_indices, transform=train_transforms)
+val_dataset = TransformSubset(val_indices, transform=valid_transforms)
+
+
